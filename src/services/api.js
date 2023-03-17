0d5f6e8218340eafbed5b069073e2abbc2d9b5b2
guipleit <112783039+guipleit@users.noreply.github.com>
2023-03-17 16:12:30 -0300

    feat req 1

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,21 @@
 export async function getCategories() {
-  // Implemente aqui
+  // Retorna um array de objetos com id e categoria
+  const REQUEST_URL = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const RESPONSE = await REQUEST_URL.json();
+  return RESPONSE;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  // Retorna um objetão com o item buscado
+  const QUERY_URL = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
+  const QUERY_RESPONSE = await QUERY_URL.json();
+  //
+  const CATEGORY_URL = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`);
+  const CATEGORY_RESPONSE = await CATEGORY_URL.json();
+  return (
+    QUERY_RESPONSE,
+    CATEGORY_RESPONSE
+  );
 }
 
 export async function getProductById() {
