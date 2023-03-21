154632873db076e8753a79aeb7614aa65482099b
Vinícius Souza <tradutorvinicius@gmail.com>
2023-03-21 15:45:28 -0300

    Quase resolve requisito 10

--- src/Pages/ShoppingCart.js
+++ src/Pages/ShoppingCart.js
@@ -5,42 +5,70 @@ class ShoppingCart extends Component {
   constructor() {
     super();
 
-    const cartItems = JSON.parse(localStorage.getItem('products')) || [];
-    console.log(cartItems);
+    // const cartItems = JSON.parse(localStorage.getItem('products')) || [];
+
     this.state = {
       cart: getProductLocalStorage(),
     };
   }
 
+  handleIncrement = ({ target }) => {
+    const { cart } = this.state;
+
+    const { id } = target;
+
+    const newCart = cart.map((item) => {
+      if (id === item.id) {
+        return { ...item, quantity: item.quantity + 1 };
+      }
+
+      return item;
+    });
+
+    this.setState({
+      cart: newCart,
+    });
+
+    localStorage.setItem('products', JSON.stringify(cart));
+
+    console.log(cart);
+  };
+
   render() {
     const { cart } = this.state;
-    // A key utilizada no map é o index pois existe a possibilidade do usuario comprar mais de um produto igual.
-    // const mappedProductList = cart.map((product, index) => (
-    //   <div key={ index }>
-    //     <img src={ product.thumbnail } alt={ product.title } />
-    //     <h2 data-testid="shopping-cart-product-name">{product.title}</h2>
-    //     <p>{product.price}</p>
-    //     <p
-    //       data-testid="shopping-cart-product-quantity"
-    //     >
-    //       {/* Filtra o array do localStorage para identificar quantos produtos do mesmo foram adcionados ao carrinho. */}
-    //       {cart.filter((item) => (item.id === product.id)).length}
-    //     </p>
-    //   </div>
-    // ));
-
-    // Verifica se o carrinho está vazio, se sim renderiza mensagem
 
     const cartEmpty = cart.length === 0;
     const mappedCart = cart.map((item) => (
       <div key={ item.id }>
         <img src={ item.thumbnail } alt={ item.title } />
         <h2 data-testid="shopping-cart-product-name">{item.title}</h2>
+        <button
+          type="button"
+          data-testid="remove-product"
+          // onClick={}
+        >
+          Remover
+        </button>
         <p
           data-testid="shopping-cart-product-quantity"
         >
           {`Quantidade: ${item.quantity}`}
         </p>
+        <button
+          type="button"
+          data-testid="product-decrease-quantity"
+          // onClick={}
+        >
+          -
+        </button>
+        <button
+          id={ item.id }
+          type="button"
+          data-testid="product-increase-quantity"
+          onClick={ this.handleIncrement }
+        >
+          +
+        </button>
         <p>{`Preço: ${item.price}`}</p>
         <p />
       </div>
